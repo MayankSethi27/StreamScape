721c8df0b244de06be59f1868fb3c9786d6a403f
Mayank <mayanksethi324@gmail.com>
2023-03-30 19:07:17 +0530

    layout and partials created

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express=require('express');
 const app=express();
 const port=8000;
+const path=require('path');
 
 //use express router
  app.use('/',require('./routes/route'));
-// const router=require('./routes/route'); we cannot do this bcz we need to use it by app also
+// const router=require('./routes/route'); we cannot do this bcz we need to use it by app -also
 
+//setup the view engine
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
 
 
 
