c67c243449107ee0fde4e829b2ad46f82bbea9aa
Mayank <mayanksethi324@gmail.com>
2023-07-16 16:09:46 +0530

    update StreamScape

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const env=require('./config/environment');
+// const env=require('./config/environment');
 const logger=require('morgan');
 const app=express();
 require('./config/view-helpers.js')(app);
