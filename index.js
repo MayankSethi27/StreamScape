872b1f21e12c1a16560246e2d24c921bf5f25a2c
Mayank <mayanksethi324@gmail.com>
2023-07-18 21:12:15 +0530

    update index file

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const logger=require('morgan');
 const app=express();
+const env=require('./config/environment');
 require('./config/view-helpers.js')(app);
 const port=5959;
 const path=require('path');
