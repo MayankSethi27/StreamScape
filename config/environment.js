ce164d5fac5a672906e439b5f9184abad5b73ae4
Mayank <mayanksethi324@gmail.com>
2023-07-21 16:13:24 +0530

    index changes

--- config/environment.js
+++ config/environment.js
@@ -20,22 +20,22 @@ const development={
 
 const production={
     name:"production",
-    session_cookie_key:process.env.StreamScape_SESSION_COOKIE_KEY,
-    db:process.env.StreamScape_DB,
+    session_cookie_key:process.env.CODEIAL_SESSION_COOKIE_KEY,
+    db:process.env.CODEIAL_DB,
     smtp:{
         service:'gmail',
         host:"smtp.gmail.com",
         port:587,
         secure:'false',
         auth:{
-            user:process.env.StreamScape_GMAIL_USERNAME,
-            pass:process.env.StreamScape_GMAIL_PASSWORD
+            user:process.env.CODEIAL_GMAIL_USERNAME,
+            pass:process.env.CODEIAL_GMAIL_PASSWORD
         }
     },
-    google_client_id:process.env.StreamScape_GOOGLE_CLIENT_ID,
-    google_client_secret:process.env.StreamScape_GOOGLE_CLIENT_SECRET,
-    google_callback_url:process.env.StreamScape_GOOGLE_CALLBACK_URL,
-    jwt_secret:process.env.StreamScape_JWT_SECRET
+    google_client_id:process.env.CODEIAL_GOOGLE_CLIENT_ID,
+    google_client_secret:process.env.CODEIAL_GOOGLE_CLIENT_SECRET,
+    google_callback_url:process.env.CODEIAL_GOOGLE_CALLBACK_URL,
+    jwt_secret:process.env.CODEIAL_JWT_SECRET
 }
 
-module.exports=eval(process.env.StreamScape_ENVIRONMENT==undefined ? development : eval(process.env.StreamScape_ENVIRONMENT));
+module.exports=eval(process.env.CODEIAL_ENVIRONMENT==undefined ? development : eval(process.env.CODEIAL_ENVIRONMENT));
