bf75ba70b88e434fbecea25619322ef3e2c13ba0
MayankSethi27 <128615996+MayankSethi27@users.noreply.github.com>
2023-07-26 22:31:27 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,71 @@
 const gulp = require('gulp');
-//const sass = require('gulp-sass')(require('sass'));
-//will compress css file
+//used to minify css file
 const cssnano = require('cssnano');
 //used in Gulp tasks to append a unique hash to the filenames of static assets such as CSS, JavaScript, and image files.
 const rev = require('gulp-rev');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
 
+// Task for minimizing CSS files
+gulp.task('css', function(done) {
+  console.log('minifying css...');
 
-//task for minimize css file
-gulp.task('css', function(done){
-    console.log('minifying css...');
-    gulp.src('./assets/css/**/*.css')
-   
+  gulp.src('./assets/css/**/*.css')
     .pipe(cssnano())
-    .pipe(gulp.dest('./assets.css'));
-
-     gulp.src('./assets/**/*.css')
+    .pipe(gulp.dest('./assets'))
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
+    .pipe(gulp.dest('./public/assets/css'))
     .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
+      cwd: 'public',
+      merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
-    done();
+
+  done();
 });
 
+// Task for minimizing JS files
+gulp.task('js', function(done) {
+  console.log('minifying js...');
 
-gulp.task('js', function(done){
-    console.log('minifying js...');
-     gulp.src('./assets/**/*.js')
+  gulp.src('./assets/**/*.js')
     .pipe(uglify())
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
+    .pipe(gulp.dest('./public/assets/js'))
     .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
+      cwd: 'public',
+      merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
-    done()
+
+  done();
 });
 
+// Task for compressing images
+gulp.task('images', function(done) {
+  console.log('compressing images...');
 
-gulp.task('images', function(done){
-    console.log('compressing images...');
-    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+  gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
     .pipe(imagemin())
     .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
+    .pipe(gulp.dest('./public/assets/images'))
     .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
+      cwd: 'public',
+      merge: true
     }))
     .pipe(gulp.dest('./public/assets'));
-    done();
+
+  done();
 });
 
-
-// empty the public/assets directory
-//Whenever server will restart all old work done by gulp will be deleted and it will perform all the tasks and minificatin again
-gulp.task('clean:assets', function(done){
-   
-    done();
+// Empty the public/assets directory
+gulp.task('clean:assets', function(done) {
+  // Add cleaning logic if required
+  done();
 });
 
 // Call the tasks in series
-//Run all the taks one by one 
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
-    console.log('Building assets');
-    done();
-});
-  
-  
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images', function(done) {
+  console.log('Building assets');
+  done();
+}));
