788ee34ff266f263c780b8318f1fcddee4590092
MayankSethi27 <128615996+MayankSethi27@users.noreply.github.com>
2023-07-26 22:37:45 +0530

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,60 @@
 const gulp = require('gulp');
-const cssnano = require('cssnano');
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
-
-// Task for minimizing CSS files
-gulp.task('css', function() {
-  console.log('minifying css...');
-
-  return gulp.src('./assets/css/**/*.css')
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets'))
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/css'))
-    .pipe(rev.manifest({
-      cwd: 'public',
-      merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-
+// const del = require('del');
+gulp.task('css', function(done){
+console.log('minifying css...');
+gulp.src('./assets/sass/**/*.scss')
+.pipe(sass())
+.pipe(cssnano())
+.pipe(gulp.dest('./assets.css'));
+gulp.src('./assets/**/*.css')
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done();
 });
-
-// Task for minimizing JS files
-gulp.task('js', function() {
-  console.log('minifying js...');
-
-  return gulp.src('./assets/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/js'))
-    .pipe(rev.manifest({
-      cwd: 'public',
-      merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-
+gulp.task('js', function(done){
+console.log('minifying js...');
+gulp.src('./assets/**/*.js')
+.pipe(uglify())
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done()
 });
-
-// Task for compressing images
-gulp.task('images', function() {
-  console.log('compressing images...');
-
-  return gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-    .pipe(imagemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets/images'))
-    .pipe(rev.manifest({
-      cwd: 'public',
-      merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-
+gulp.task('images', function(done){
+console.log('compressing images...');
+gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+.pipe(imagemin())
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done();
 });
-
-// Empty the public/assets directory
-gulp.task('clean:assets', function(done) {
-  // Add cleaning logic if required
-  done();
+// empty the public/assets directory
+gulp.task('clean:assets', function(done){
+// del.sync('./public/assets');
+done();
 });
-
 // Call the tasks in series
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images', function(done) {
-  console.log('Building assets');
-  done();
-}));
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'),
+function(done){
+console.log('Building assets');
+done();
+});
