fd5de43a537cc6914c7b69921edda6d94d1ae7ac
Mayank <mayanksethi324@gmail.com>
2023-07-26 22:42:08 +0530

    install gulp-css dependency

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,60 @@
 const gulp = require('gulp');
-//const sass = require('gulp-sass')(require('sass'));
-//will compress css file
-const cssnano = require('cssnano');
-//used in Gulp tasks to append a unique hash to the filenames of static assets such as CSS, JavaScript, and image files.
+const sass = require('gulp-sass')(require('sass'));
+const cssnano = require('gulp-cssnano');
 const rev = require('gulp-rev');
 const uglify = require('gulp-uglify-es').default;
 const imagemin = require('gulp-imagemin');
-
-
-//task for minimize css file
+// const del = require('del');
 gulp.task('css', function(done){
-    console.log('minifying css...');
-    gulp.src('./assets/css/**/*.css')
-   
-    .pipe(cssnano())
-    .pipe(gulp.dest('./assets.css'));
-
-     gulp.src('./assets/**/*.css')
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
+console.log('minifying css...');
+gulp.src('./assets/sass/**/*.scss')
+.pipe(sass())
+.pipe(cssnano())
+.pipe(gulp.dest('./assets.css'));
+gulp.src('./assets/**/*.css')
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done();
 });
-
-
 gulp.task('js', function(done){
-    console.log('minifying js...');
-     gulp.src('./assets/**/*.js')
-    .pipe(uglify())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done()
+console.log('minifying js...');
+gulp.src('./assets/**/*.js')
+.pipe(uglify())
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done()
 });
-
-
 gulp.task('images', function(done){
-    console.log('compressing images...');
-    gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
-    .pipe(imagemin())
-    .pipe(rev())
-    .pipe(gulp.dest('./public/assets'))
-    .pipe(rev.manifest({
-        cwd: 'public',
-        merge: true
-    }))
-    .pipe(gulp.dest('./public/assets'));
-    done();
+console.log('compressing images...');
+gulp.src('./assets/**/*.+(png|jpg|gif|svg|jpeg)')
+.pipe(imagemin())
+.pipe(rev())
+.pipe(gulp.dest('./public/assets'))
+.pipe(rev.manifest({
+cwd: 'public',
+merge: true
+}))
+.pipe(gulp.dest('./public/assets'));
+done();
 });
-
-
 // empty the public/assets directory
-//Whenever server will restart all old work done by gulp will be deleted and it will perform all the tasks and minificatin again
 gulp.task('clean:assets', function(done){
-   
-    done();
+// del.sync('./public/assets');
+done();
 });
-
 // Call the tasks in series
-//Run all the taks one by one 
-gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'), function(done){
-    console.log('Building assets');
-    done();
+gulp.task('build', gulp.series('clean:assets', 'css', 'js', 'images'),
+function(done){
+console.log('Building assets');
+done();
 });
-  
-  
